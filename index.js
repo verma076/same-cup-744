9fe298d83d329be19b039f32ab5ed9b84bc31bf5
verma076 <nvmasai0706@gmail.com>
2023-01-22 01:59:46 +0530

    day2

---
+++ index.js
@@ -0,0 +1,8 @@
+var sign=document.getElementById("sign")
+sign.addEventListener("click",()=>{
+    window.location.href="./sign in.html"
+})
+var signup=document.getElementById("signup")
+signup.addEventListener("click",()=>{
+    window.location.href="./signup.html"
+})
